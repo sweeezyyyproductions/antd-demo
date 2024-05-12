fe2ee2881d55d09f513f3559af201ba923aeb8d1
Michael Simpson <100339112+sweeezyyyproductions@users.noreply.github.com>
2024-05-12 01:21:33 -0700

    refactor: Remove unused components and files

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ConfigProvider, Button, Space, Input, Divider } from 'antd';
 import { Layout } from "antd";
 import { Rate } from 'antd';
-import TextEdit from './components/TextEdit';
-import NavBar from './components/navBar';
 import PodcastTool from './components/podcastTool';
 
 const App: React.FC = () => (
@@ -11,8 +9,6 @@ const App: React.FC = () => (
     
       <div>Name Generator V1
       <PodcastTool/>
-      {/* <NavBar/> */}
-        {/* <TextEdit/> */}
       </div>
     
   </>
